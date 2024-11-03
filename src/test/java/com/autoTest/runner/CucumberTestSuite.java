8f616f334cb63270a9b115cb79f0b3e2b2ae648b
Hi <nguyenquocanh20499@gmail.com>
2024-11-03 22:35:02 +0700

    update

--- src/test/java/com/autoTest/runner/CucumberTestSuite.java
+++ src/test/java/com/autoTest/runner/CucumberTestSuite.java
@@ -1,10 +1,21 @@
 package com.autoTest.runner;
 
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+//@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,json:target/cucumber-report/cucumber.json,html:target/cucumber-report")
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@smoke")
+//@ConfigurationParameter(key = FILTER_NAME_PROPERTY_NAME, value = "Sample Scenario")
+//@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
+//@SelectFiles({
+//        @SelectFile("src/test/resources/features/feature1.feature"),
+//        @SelectFile("src/test/resources/features/feature2.feature")
+//})
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features")
-public class CucumberTestSuite {}
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@ValidCredentials")
+public class CucumberTestSuite {
+}
